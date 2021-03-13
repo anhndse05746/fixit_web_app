c5f0f55404d7fa1c7b6e3956e73ae197fa2c5cb6
NguyenMinhDuc98 <ottffssent9897@gmail.com>
2021-03-13 14:49:06 +0700

    setup redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './pages/Home';
 import LoginPage from './components/Login';
 import "./assets/FontAwsomeIcons/icon";
 
